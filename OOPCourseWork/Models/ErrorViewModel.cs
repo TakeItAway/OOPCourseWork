9bc560713f41ceeca03f6b71d1e2ac3a2e782189
wowaag <wowaag@DESKTOP-V6UL8SA>
2018-05-11 19:28:08 +0300

    Добавьте файлы проекта.

---
+++ OOPCourseWork/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OOPCourseWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
